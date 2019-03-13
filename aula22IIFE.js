d91a5401085d8f9029065464dc4580e98d44e519
Felipe Machado <felipe.machado.ads@gmail.com>
2019-03-12 22:42:01 -0300

    aula 22 = IIFE ,maneira de fugir do escopo global

---
+++ aula22IIFE.js
@@ -0,0 +1,8 @@
+//IIFE -> immediately invoked function expression
+
+(function(){
+    console.log('Será invocada na hora!')
+    console.log('Foge do escopo mais abrangente')
+})();
+
+//Foge escopo global
